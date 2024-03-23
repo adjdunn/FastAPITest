422ec8c27e38fbe34a0aaeb477297a99321888aa
Aaron Dunn <wiresift@gmail.com>
2024-03-22 20:56:42 -0700

    Added CORS middleware config

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 # Import FastAPI
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Header, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 from db_functions import get_data, api_call
 
 # Create an instance of the FastAPI class
 app = FastAPI()
 
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins for demonstration; specify your domain in production
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 # Define a root endpoint that handles GET requests
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-# Define a path operation for /items/{item_id}
-@app.get("/news")
+
+def validate_api_key(api_key: str = Header(...)):
+    # Replace 'your_api_key' with your validation logic
+    if api_key != "your_api_key":
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+
+
+
+@app.get("/news", dependencies=[Depends(validate_api_key)])
 def get_news(sectors: Optional[str] = None,
              regions: Optional[str] = None,
              sizes: Optional[str] = None,
